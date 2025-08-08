d12aacb33f7dc8f04cda8bbc0da78459e5eb0f4b
thinkcode <you@example.comthinkcodellc@gmail.com>
2025-08-08 15:29:41 -0500

    Fix issues related to vercel build

--- chicbysally-nextjs/src/app/layout.tsx
+++ chicbysally-nextjs/src/app/layout.tsx
@@ -33,7 +33,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider
+    <ClerkProvider 
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
       appearance={{
         variables: { colorPrimary: '#f59e0b' },
         elements: {
